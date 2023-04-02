77dbdee4dd37a5d89b5ff957384f7e79787065fd
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-04-02 21:04:59 +0530

    proper commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   const checkStatus = () => {
     setLoading(true);
-    fetch(`http://localhost:5000/user/${username}`)
+    fetch(`https://instapi.akshayk.dev/user/${username}`)
       .then(response => response.json())
       .then(data => {
         setStatus(data.prediction ? 'fake' : 'real');
@@ -25,14 +25,19 @@ function App() {
   
   
   return (
-    <div>
-    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-    <button onClick={checkStatus}>Check Status</button>
-    {loading && <div>Loading...</div>}
-    {status === 'fake' && <div>Fake Account</div>}
-    {status === 'real' && <div>Real Account</div>}
-    {status === 'error' && <div>Server Error</div>}
+    <div className='min'>
+    <div className='container'>
+      
+<h3 className="usr">Username</h3>
+    <input className='usrname' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+    <button className='btn' onClick={checkStatus}>Check Status</button>
+    {loading && <div className='status' >Loading...</div>}
+    {status === 'fake' && <div className='status'>Fake Account</div>}
+    {status === 'real' && <div className='status'>Real Account</div>}
+    {status === 'error' && <div className='status'>Server Error</div>}
   </div>
+  </div>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import new1 from './new1'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <new1/> 
     <App />
   </React.StrictMode>,
 )
